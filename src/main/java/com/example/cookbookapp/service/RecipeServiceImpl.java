671a32182687fcf1192160729430122f1716103e
Bohdan Senyk <senyk2001@gmail.com>
2023-04-05 20:07:02 +0300

    removed unnecessary imports

--- src/main/java/com/example/cookbookapp/service/RecipeServiceImpl.java
+++ src/main/java/com/example/cookbookapp/service/RecipeServiceImpl.java
@@ -2,10 +2,8 @@ package com.example.cookbookapp.service;
 
 import com.example.cookbookapp.model.Recipe;
 import com.example.cookbookapp.repository.RecipeRepository;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
